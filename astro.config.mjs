efd8964d2a9cb6bdaab2fcd1462e4457a109df54
Dylan Bateman <dylanbateman25@outlook.com>
2025-04-09 20:34:56 +0100

    Added cloudflare bits

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import mdx from '@astrojs/mdx'
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     
   },
+
   integrations: [mdx()],
+  adapter: cloudflare(),
 })
